19e6d30177187e94451fcb93d10af3fb1eea6d6e
Egor Kochetkov <pirs2132@gmail.com>
2025-08-31 16:27:52 +0400

    refactor: move GetWarehouseByID method to a new file for better organization

--- microservice-warehouses/internal/handler/grpc/grpc.go
+++ microservice-warehouses/internal/handler/grpc/grpc.go
@@ -1,11 +1,8 @@
 package grpc
 
 import (
-	"context"
-
 	"github.com/PIRSON21/grpc-microservices/microservice-warehouses/internal/service"
 	pb "github.com/PIRSON21/grpc-microservices/microservice-warehouses/proto"
-	"github.com/sirupsen/logrus"
 )
 
 type WarehouseHandler struct {
@@ -17,40 +14,4 @@ func NewWarehouseHandler(service *service.WarehouseService) *WarehouseHandler {
 	return &WarehouseHandler{
 		service: service,
 	}
-}
-
-// GetWarehouseByID handles gRPC requests to retrieve a warehouse by its ID.
-func (h *WarehouseHandler) GetWarehouseByID(ctx context.Context, req *pb.GetWarehouseRequest) (*pb.WarehouseResponse, error) {
-	log := logrus.WithFields(logrus.Fields{
-		"op": "handler.grpc.WarehouseHandler.GetWarehouseByID",
-		"id": req.Id,
-	})
-
-	log.Debug("Received gRPC request to get warehouse by ID")
-
-	if req.Id == "" {
-		log.Warn("Received empty ID in request")
-		return &pb.WarehouseResponse{}, nil
-	}
-
-	warehouses, err := h.service.GetWarehouses(ctx, req.Id)
-	if err != nil {
-		log.Errorf("Error retrieving warehouse: %v", err)
-		return nil, err
-	}
-
-	if len(warehouses) == 0 {
-		log.Warn("No warehouse found with the given ID")
-		return &pb.WarehouseResponse{}, nil
-	}
-
-	warehouse := warehouses[0]
-	resp := &pb.WarehouseResponse{
-		Id:      warehouse.ID,
-		Address: warehouse.Address,
-	}
-
-	log.Debug("Successfully retrieved warehouse by ID")
-
-	return resp, nil
 }
